2a0b541d610b2b12c6a011de9b934550c88f8d74
Killswitch5150 <92342531+Killswitch5150@users.noreply.github.com>
2025-02-07 03:27:16 +0000

    added main py file

---
+++ fping.py
@@ -0,0 +1,25 @@
+#fping.py is a ping dependent streamlined version of ping. it saves users typing "-c" and that is it.
+#originally written by WebDevPro for a tutorial, and reworked by me in September 2022 to run on OS X
+#i now have added cli support to it
+
+#example cli call "python3 fping.py 8.8.8.8 -4"
+
+import argparse  #library used to allow CLI arguments
+import os  #used to allow system interaction
+
+# argparse initialization
+parser = argparse.ArgumentParser(description="fping is a slightly faster ping script. saving users an average of typing one letter.")  #appears with --help
+parser.add_argument('ip_target', type=str, help="Target IP Address to ping")  #argument for IP address
+parser.add_argument('-c', '--count', type=int, default=4, help="Number of packets (default is 4)")  #argument for packet count
+
+args = parser.parse_args() #saves cli input to args datatype (list?)
+
+# Access the arguments
+ip_target = args.ip_target  #saves the IP target info from the CLI
+ping_count = args.count  #saves the packet count from the CLI
+
+print(f"Sending {ping_count} packets to {ip_target}") #print feedback for troubleshooting/confirmation
+
+os.system(f'ping -c {ping_count} {ip_target}') #runs ping command
+
+input("Press any Key to Exit") #placeholder to keep console open when running standalone
